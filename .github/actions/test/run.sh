0dcf522b80076ea2fddeb12673f44508df0dedd0
Victor Sandoval <stb.4421@gmail.com>
2024-01-28 23:29:02 +0000

    fix: typo in test action run script

--- .github/actions/test/run.sh
+++ .github/actions/test/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "Installing project..."
-- poetry install --with dev
+poetry install --with dev
 echo "Running tests..."
 poetry run coverage run -m pytest tests --full-trace
 poetry run coverage report -m -i
