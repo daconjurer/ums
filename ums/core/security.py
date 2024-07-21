bc5aa2d5b1f87a6ca066439296ef0e4a580f49e2
Victor Sandoval <vs.software.eng@gmail.com>
2024-07-21 21:14:29 +0100

    chore: replace passlib with bcrypt

--- ums/core/security.py
+++ ums/core/security.py
@@ -1,27 +1,21 @@
 from datetime import datetime, timedelta
 
-import logging
 from jose import jwt
-from passlib.context import CryptContext
+import bcrypt
 
 from ums.settings.application import get_app_settings
 
 
 security_settings = get_app_settings().security
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
-def verify_password(plain_password, password) -> bool:
-    logging.getLogger().setLevel(logging.ERROR)
-    # Silence "AttributeError: module 'bcrypt' has no attribute '__about__'" warning log
-    verified = pwd_context.verify(plain_password, password)
-    logging.getLogger().setLevel(logging.INFO)
+def verify_password(plain_password, hashed_password) -> bool:
+    verified = bcrypt.checkpw(plain_password.encode(), hashed_password.encode())
     return verified
 
 
 def get_password_hash(password: str):
-    return pwd_context.hash(password)
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
 
 
 def create_access_token(data: dict, expire_delta: timedelta | None = None):
