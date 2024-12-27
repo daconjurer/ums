8b083d45bed793d7c6d8f32509490febbf82a5ab
Victor Sandoval <vs.software.eng@gmail.com>
2024-12-27 07:57:04 -0600

    chore: ignore type checking on 'api_host' address setting

--- ums/settings/api.py
+++ ums/settings/api.py
@@ -5,6 +5,6 @@ from ums.settings.base import CommonSettings
 
 class APISettings(CommonSettings):
     api_host: IPvAnyAddress = Field(
-        validation_alias="API_HOST", default=IPvAnyAddress("0.0.0.0")
+        validation_alias="API_HOST", default=IPvAnyAddress("0.0.0.0")  # type: ignore
     )
     api_port: int = Field(validation_alias="API_PORT", default=3000)
