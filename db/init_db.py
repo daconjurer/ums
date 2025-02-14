a70da44e10c5aa2067fc35e6a1e742af6c79f5d6
Victor Sandoval <vs.software.eng@gmail.com>
2025-02-14 21:50:29 +0000

    chore: format

--- db/init_db.py
+++ db/init_db.py
@@ -1,14 +1,13 @@
 import asyncio
 from uuid import UUID
 
-from ums.core.utils.security import get_password_hash
 from ums.core.db.async_session import (
     DatabaseManager,
     create_custom_engine,
     get_async_session,
 )
+from ums.core.utils.security import get_password_hash
 from ums.domain.entities import Group, Permissions, Role, User
-
 from ums.settings.application import get_app_settings
 
 db_settings = get_app_settings().db
