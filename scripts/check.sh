99a645348b626364d565dc7db1cb344214acc04c
Victor Sandoval <vs.software.eng@gmail.com>
2025-02-14 22:19:41 +0000

    chore: allow project to run on Python 3.12; revert makefile-based checks

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,25 +1,25 @@
 #! /bin/bash
 
-echo "Running ruff checks..."
-make lint
+echo "ruff check --diff ums/ tests/ db/ && ruff check --select I --diff ums/ tests/ db/"
+ruff check --diff ums/ tests/ db/ && ruff check --select I --diff ums/ tests/ db/
 if [ $? -ne 0 ]; then
-    echo "Error: ruff checks failed"
+    echo "Error: ruff check failed"
     exit 1
 fi
 echo "DONE!"
 
-echo "Running ruff format..."
-make format
+echo "ruff format --diff ums/ tests/ db/"
+ruff format --diff ums/ tests/ db/
 if [ $? -ne 0 ]; then
     echo "Error: ruff format failed"
     exit 1
 fi
 echo "DONE!"
 
-echo "Running type checks..."
-make typecheck
+echo "mypy ums/ tests/ db/"
+mypy ums/ tests/ db/
 if [ $? -ne 0 ]; then
-    echo "Error: type checking failed"
+    echo "Error: mypy failed"
     exit 1
 fi
 echo "DONE!"
