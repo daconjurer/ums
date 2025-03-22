91eebe31a93b3088fa7412cf419112118de254db
Victor Sandoval <vs.software.eng@gmail.com>
2025-03-22 16:19:56 +0000

    fix: revert 'tests' workflow to original state; remove incorrect 'test' action

--- .github/actions/test/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-echo "Installing project..."
-poetry install -v
-echo "Running tests..."
-make test
-echo "Tests completed successfully"
-echo "Coverage report..."
-make coverage
